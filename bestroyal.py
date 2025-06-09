45b418e21204c2d2b63b522a87d438b8abfee220
royal865 <royal.bullet.7773@gmail.com>
2025-06-09 21:30:36 +0530

    Update bestroyal.py

--- bestroyal.py
+++ bestroyal.py
@@ -397,11 +397,11 @@ async def attack(update: Update, context: CallbackContext):
 
     # Determine the attack command based on the argument type
     if argument_type == 3:
-        attack_command = f"./ROYAL {ip} {port} {duration}"
+        attack_command = f"./THAKUR {ip} {port} {duration}"
     elif argument_type == 4:
-        attack_command = f"./ROYAL {ip} {port} {duration} {threads}"
+        attack_command = f"./THAKUR {ip} {port} {duration} {threads}"
     elif argument_type == 5:
-        attack_command = f"./ROYAL {ip} {port} {duration} {byte_size} {threads}"
+        attack_command = f"./THAKUR {ip} {port} {duration} {byte_size} {threads}"
 
     # Send attack details to the user
     await context.bot.send_message(chat_id=chat_id, text=( 
